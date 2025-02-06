f35eb2847df31ca89724e5c604ee2c90daf57401
James Frost <git@frost.cx>
2025-02-06 22:55:52 +0000

    Fixes and ruff

--- gelbooru_downloader.py
+++ gelbooru_downloader.py
@@ -6,64 +6,64 @@
 import requests
 from urllib.parse import unquote
 import time
-#import argparse
-
-
-#parser = argparse.ArgumentParser(prog="yande.re-downloader", description="Download images from yande.re")
-#parser.add_argument("input file", type=path, help="a file from which URLs can be taken")
-#parser.add_argument(["-w","--wait"], type=int, default=3, help="number of seconds to wait between #operations")
-#args = parser.parse_args()
-
+import random
 
 
 def get_urls_from_file(input_file):
-	with open("download") as f:
-		return [line if line.startswith("https://gelbooru.com/") in (l.strip() for l in f.readlines())]
+    with open("download") as f:
+        return list(
+            filter(
+                lambda l: l.startswith("https://gelbooru.com/"),
+                (l.strip() for l in f.readlines()),
+            )
+        )
 
 
 def get_page_html(page_url):
-	print(page_url)
-	r = requests.get(page_url)
-	html_page = r.text
-	return html_page
+    r = requests.get(page_url)
+    html_page = r.text
+    return html_page
 
 
 def get_image_url(html_page):
-	# The full res image link has the text "Original image".
-	raw_image_url = ""
-	for line in html_page.splitlines():
-		for subline in line.split("<li>"):
-			if 'Original image' in subline:
-				raw_image_url = subline
-	image_url = raw_image_url.split(" ")
-	for part in image_url:
-		if 'href=' in part:
-			image_url = part.split('"')
-	for part in image_url:
-		if 'http' in part:
-			image_url = part
-	print(image_url)
-	#input("ENTER to continue.")
-	return image_url
+    # The full res image link has the text "Original image".
+    raw_image_url = ""
+    for line in html_page.splitlines():
+        for subline in line.split("<li>"):
+            if "Original image" in subline:
+                raw_image_url = subline
+    image_url = raw_image_url.split(" ")
+    for part in image_url:
+        if "href=" in part:
+            image_url = part.split('"')
+    for part in image_url:
+        if "http" in part:
+            image_url = part
+    return image_url
 
 
 def download_image(image_url):
-	r = requests.get(image_url)
-	if r.status_code == 200:
-		print("Downloaded: {}".format(r.status_code))
-		filename = unquote(image_url.split('/')[-1])
-		with open(filename,'wb') as output_file:
-			output_file.write(r.content)
-	else:
-		print("Download Failed: {}".format(r.status_code))
-	time.sleep(3)
+    r = requests.get(image_url)
+    if r.status_code == 200:
+        print(f"Downloaded: {r.status_code}")
+        filename = unquote(image_url.split("/")[-1])
+        with open(filename, "wb") as output_file:
+            output_file.write(r.content)
+    else:
+        print(f"Download Failed: {r.status_code}")
+    # Randomly wait between 2 and 5 seconds.
+    time.sleep(2 + 3 * random.random())
 
 
 def page_url_to_image(page_url):
-	download_image(get_image_url(get_page_html(page_url)))
+    print(f"Fetching HTML for {page_url}")
+    page_html = get_page_html(page_url)
+    image_url = get_image_url(page_html)
+    print(f"Fetching image from {image_url}")
+    download_image(image_url)
 
 
 for url in get_urls_from_file("file"):
-	page_url_to_image(url.strip())
+    page_url_to_image(url)
 
 print("███████████████\n███  DONE!  ███\n███████████████")
--- yande.re_downloader.py
+++ yande.re_downloader.py
@@ -8,69 +8,68 @@ from urllib.parse import unquote
 import time
 
 from requests.models import HTTPError
-#import argparse
+# import argparse
 
 
-#parser = argparse.ArgumentParser(prog="yande.re-downloader", description="Download images from yande.re")
-#parser.add_argument("input file", type=path, help="a file from which URLs can be taken")
-#parser.add_argument(["-w","--wait"], type=int, default=3, help="number of seconds to wait between #operations")
-#args = parser.parse_args()
-
+# parser = argparse.ArgumentParser(prog="yande.re-downloader", description="Download images from yande.re")
+# parser.add_argument("input file", type=path, help="a file from which URLs can be taken")
+# parser.add_argument(["-w","--wait"], type=int, default=3, help="number of seconds to wait between #operations")
+# args = parser.parse_args()
 
 
 def get_urls_from_file(input_file):
-	with open("download") as f:
-		return [line.strip() for line in f.readlines() if line[0] == 'h']
+    with open("download") as f:
+        return [line.strip() for line in f.readlines() if line[0] == "h"]
 
 
 def get_page_html(page_url):
-	print(page_url)
-	r = requests.get(page_url)
-	html_page = r.text
-	return html_page
+    print(page_url)
+    r = requests.get(page_url)
+    html_page = r.text
+    return html_page
 
 
 def get_image_url(html_page):
-	# The full res image link is either tagged 'id="highres"' or 'id="png"'; PNG is preferred.
-	raw_image_url = ""
-	for line in html_page.splitlines():
-		if 'id="png"' in line:
-			raw_image_url = line
-		elif 'id="highres"' in line:
-			raw_image_url = line
-	image_url = raw_image_url.split(" ")
-	for part in image_url:
-		if 'href=' in part:
-			image_url = part.split('"')
-	for part in image_url:
-		if 'http' in part:
-			image_url = part
-	print(image_url)
-	if image_url[0] != 'h':
-		image_url = "https://frost.cx/aholuiefswvofhsrugreg/notarealpage.bz2"
-	return image_url
+    # The full res image link is either tagged 'id="highres"' or 'id="png"'; PNG is preferred.
+    raw_image_url = ""
+    for line in html_page.splitlines():
+        if 'id="png"' in line:
+            raw_image_url = line
+        elif 'id="highres"' in line:
+            raw_image_url = line
+    image_url = raw_image_url.split(" ")
+    for part in image_url:
+        if "href=" in part:
+            image_url = part.split('"')
+    for part in image_url:
+        if "http" in part:
+            image_url = part
+    print(image_url)
+    if image_url[0] != "h":
+        image_url = "https://frost.cx/aholuiefswvofhsrugreg/notarealpage.bz2"
+    return image_url
 
 
 def download_image(image_url):
-	try:
-		r = requests.get(image_url)
-		r.raise_for_status()
-		filename = unquote(image_url.split('/')[-1])
-		with open(filename,'wb') as output_file:
-			output_file.write(r.content)
-	except HTTPError:
-		print("HTTP error: Status code {0} from {1}".format(r.status_code, r.url))
-	except:
-		print("An unknown issue occurred with URL {}".format(r.url))
-	print("Downloaded!")
-	time.sleep(3)
+    try:
+        r = requests.get(image_url)
+        r.raise_for_status()
+        filename = unquote(image_url.split("/")[-1])
+        with open(filename, "wb") as output_file:
+            output_file.write(r.content)
+    except HTTPError:
+        print("HTTP error: Status code {0} from {1}".format(r.status_code, r.url))
+    except:
+        print("An unknown issue occurred with URL {}".format(r.url))
+    print("Downloaded!")
+    time.sleep(3)
 
 
 def page_url_to_image(page_url):
-	download_image(get_image_url(get_page_html(page_url)))
+    download_image(get_image_url(get_page_html(page_url)))
 
 
 for url in get_urls_from_file("file"):
-	page_url_to_image(url.strip())
+    page_url_to_image(url.strip())
 
 print("███████████████\n███  DONE!  ███\n███████████████")
